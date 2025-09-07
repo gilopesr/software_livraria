a0c2f267a6de66cea53d3e85a51b76f54cedc23d
LilianBria <lilianbria@yahoo.com.br>
2025-09-06 22:21:18 -0300

    Primeiro commit do projeto

--- config.py
+++ config.py
@@ -2,12 +2,18 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 
 app.config['HOST'] = '0.0.0.0'
 app.config['PORT']=5000
 app.config['DEBUG'] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///livraria.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/livraria"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+
+if __name__ == "__main__":
+       app.run()
