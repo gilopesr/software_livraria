55485c3e27f1c5fe4953d5c44618de25ae5ac805
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-11-10 22:55:16 -0300

    feat:começo da funcionalidade de carrinho

--- static/index.js
+++ static/index.js
@@ -343,4 +343,11 @@ window.onload = function() {
     autoTimeout = setTimeout(autoRotate, 5000);
 };
 
+var btn = document.querySelector("button.checkout-btn")
+btn.addEventListener('click', function(){
+    localtion.href = '/templates/compras.html'
+})
 
+function shoppingCart(){
+    
+}
